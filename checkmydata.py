ff15abf5607b7b48f2107156500de511dbfd32e0
QuerySavvy <145577861+QuerySavvy@users.noreply.github.com>
2023-10-04 17:29:36 +0200

    Update checkmydata.py

--- checkmydata.py
+++ checkmydata.py
@@ -1,3 +1,24 @@
 import streamlit as st
+import pandas as pd
 
-st.write("website coming soon")
+
+location = st.selectbox('Choose file type:',['Git URL','Upload a file'],)
+file = None
+
+# define the file to be imported and replace the backslash with forwardslash
+if location == 'Git URL':
+    file = st.text_input("Enter your URL below:", "https://raw.githubusercontent.com/QuerySavvy/TrainingFiles/main/Airline%20Dataset%20Updated%20-%20v2.csv")    
+    if len(file)<1:
+        st.info('☝️ Please insert a URL')
+        st.stop()
+if location == 'Upload a file':
+    file = st.file_uploader("Please choose a file to upload:")
+    if file is None:
+        st.info('☝️ Upload a file')
+        st.stop()
+    st.balloons()
+# read the file and convert ot a pandas DataFrame
+data = pd.read_csv(file)
+
+st.write("Data preview:")
+st.write(data.head())
